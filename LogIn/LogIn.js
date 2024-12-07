2a070edecc962d3ab1a86797eb48b66365343279
Eri <erireqica5@gmail.com>
2024-12-07 02:49:12 +0100

    validimi javascript

---
+++ LogIn/LogIn.js
@@ -0,0 +1,97 @@
+document.addEventListener('DOMContentLoaded', () => {
+    validimiLogIn();
+    validimiSignUp();
+});
+
+function validimiLogIn() {
+    const Submit1 = document.getElementById('submit1'); 
+
+    const validate = (ngjarja) => {
+        ngjarja.preventDefault(); 
+
+        const emaili1 = document.getElementById('emaili1');
+        const fjalkalimi1 = document.getElementById('pass1');
+
+        if (emaili1.value.trim() === "") {
+            alert("Ju lutem shtoni email-in.");
+            emaili1.focus();
+            return false;
+        }
+
+        if (!emailValid(emaili1.value)) {
+            alert("Ju lutem shtoni një email valid.");
+            emaili1.focus();
+            return false;
+        }
+
+        if (fjalkalimi1.value.trim() === "") {
+            alert("Ju lutem shtoni fjalëkalimin.");
+            fjalkalimi1.focus();
+            return false;
+        }
+
+        alert("Log In u realizua me sukses!");
+        return true;
+    };
+
+    const emailValid = (email) => {
+        const emailRegex = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+        return emailRegex.test(email.toLowerCase());
+    };
+
+    Submit1.addEventListener('click', validate);
+}
+
+
+function validimiSignUp() {
+    const Submit2 = document.getElementById('submit2'); 
+
+    const validate = (ngjarja) => {
+        ngjarja.preventDefault(); 
+
+        const emaili2 = document.getElementById('emaili2');
+        const fjalkalimi2 = document.getElementById('pass2');
+        const emrin1 = document.getElementById('emri1');
+        const mbiemri1 = document.getElementById('mbiemri1');
+
+        if (emrin1.value.trim() === "") {
+            alert("Ju lutem shtoni emrin.");
+            emrin1.focus();
+            return false;
+        }
+
+        if (mbiemri1.value.trim() === "") {
+            alert("Ju lutem shtoni mbiemrin.");
+            mbiemri1.focus();
+            return false;
+        }
+
+        if (emaili2.value.trim() === "") {
+            alert("Ju lutem shtoni email-in.");
+            emaili2.focus();
+            return false;
+        }
+
+        if (!emailValid(emaili2.value)) {
+            alert("Ju lutem shtoni një email valid.");
+            emaili2.focus();
+            return false;
+        }
+
+        if (fjalkalimi2.value.trim() === "") {
+            alert("Ju lutem shtoni fjalëkalimin.");
+            fjalkalimi2.focus();
+            return false;
+        }
+
+        alert("Sign Up u realizua me sukses!");
+        return true;
+    };
+
+    const emailValid = (email) => {
+        const emailRegex = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+        return emailRegex.test(email.toLowerCase());
+    };
+
+    Submit2.addEventListener('click', validate);
+}
